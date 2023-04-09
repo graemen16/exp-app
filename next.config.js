d9a086d91836f7946f99f1223d773966b23e4563
graemen16 <graemen16@gmail.com>
2023-04-09 16:03:45 +0100

    standalone nextConfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  output:"standalone", // Enable standalone feature to get round a limitation in Azure Static Web Aps of 100MB (see tutorial)
 }
 
 module.exports = nextConfig
